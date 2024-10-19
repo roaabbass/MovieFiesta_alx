c7fbf4013bc70fa7a664542412e5678cc802caac
Roaa <roaabbass@gmail.com>
2024-10-19 22:25:58 +0300

    where i fetch the data from the api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = '4fdd443f';  
+const BASE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4fdd443f";
+
+export const fetchMovies = async (query) => {
+  const { data } = await axios.get(`${BASE_URL}&s=${query}`);
+  return data.Search;
+};
+
+export const fetchMovieDetails = async (id) => {
+  const { data } = await axios.get(`${BASE_URL}&i=${id}`);
+  return data;
+};
