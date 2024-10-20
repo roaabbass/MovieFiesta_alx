fda2d1a78adce7b307818298ae3a8f749ab23430
Roaa <roaabbass@gmail.com>
2024-10-20 18:58:03 +0300

    Here the detials of the movie can be found

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,15 +1,24 @@
-const MovieDetails = ({ movie }) => (
-  <div className="p-6 text-white">
-    <img src={movie.Poster} alt={movie.Title} className="w-1/3 rounded-lg" />
-    <h1 className="text-4xl font-bold mt-4">{movie.Title}</h1>
-    <p className="mt-2">{movie.Plot}</p>
-    <p className="mt-1">
-      <strong>Genre:</strong> {movie.Genre}
-    </p>
-    <p>
-      <strong>Released:</strong> {movie.Released}
-    </p>
-  </div>
-);
+// src/components/MovieDetails.jsx
+const MovieDetails = ({ movie }) => {
+  return (
+    <div className="p-4 bg-primary min-h-screen text-white">
+      <img src={movie.Poster} alt={movie.Title} className="w-full rounded-md" />
+      <h1 className="text-3xl font-bold mt-4">{movie.Title}</h1>
+      <p>{movie.Plot}</p>
+      <div className="mt-4">
+        <strong>Genre:</strong> {movie.Genre}
+      </div>
+      <div className="mt-2">
+        <strong>Released:</strong> {movie.Released}
+      </div>
+      <div className="mt-2">
+        <strong>Director:</strong> {movie.Director}
+      </div>
+      <div className="mt-2">
+        <strong>Actors:</strong> {movie.Actors}
+      </div>
+    </div>
+  );
+};
 
 export default MovieDetails;
