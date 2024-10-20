273432e999fa495aaf03fb0f2d42bd677c304d6a
Roaa <roaabbass@gmail.com>
2024-10-20 19:05:49 +0300

    The trending movies,title, nav bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,23 @@
-import MovieCard from "../components/MovieCard";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { fetchMovies } from '../api';
+import MovieCard from '../components/MovieCard';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
 
+  const randomKeywords = ['Batman', 'Matrix', 'Star', 'Iron', 'Spider'];
+
   useEffect(() => {
-    axios
-      .get("https://www.omdbapi.com/?s=star&apikey=your_api_key")
-      .then((response) => setMovies(response.data.Search));
+    const randomKeyword = randomKeywords[Math.floor(Math.random() * randomKeywords.length)];
+    fetchMovies(randomKeyword).then(setMovies);
   }, []);
 
   return (
-    <div className="p-4 bg-primary min-h-screen">
-      <h2 className="text-white text-2xl font-bold">Trending</h2>
-      <div className="grid grid-cols-2 gap-4 mt-4">
+    <div className="p-6">
+      <h2 className="text-3xl font-bold text-white">Trending Movies</h2>
+      <div className="flex overflow-x-auto space-x-4 mt-4">
         {movies.map((movie) => (
-          <MovieCard key={movie.imdbID} movie={movie} />
+          <MovieCard key={movie.imdbID} movie={movie} onClick={() => {}} />
         ))}
       </div>
     </div>
