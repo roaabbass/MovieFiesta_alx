6303a686fdde9f855acfb5ee6c038438be3b2c07
Roaa <roaabbass@gmail.com>
2024-10-20 19:07:52 +0300

    where the calling of api takes place

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = '4fdd443f';  
-const BASE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4fdd443f";
+const API_KEY = '4fdd443'; 
+const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=4fdd443f';
 
 export const fetchMovies = async (query) => {
   const { data } = await axios.get(`${BASE_URL}&s=${query}`);
