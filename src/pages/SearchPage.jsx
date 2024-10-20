3cbdc3aa43f48c839899c4506e9ee602b20c89b0
Roaa <roaabbass@gmail.com>
2024-10-20 19:06:46 +0300

    the results found from search

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,22 +1,24 @@
+// src/pages/SearchPage.jsx
 import { useState } from 'react';
-import { fetchMovies } from '../api';
 import SearchBar from '../components/SearchBar';
 import MovieCard from '../components/MovieCard';
 
 const SearchPage = () => {
   const [movies, setMovies] = useState([]);
-
-  const handleSearch = (query) => {
-    fetchMovies(query).then(setMovies);
-  };
+  const [error, setError] = useState("");
 
   return (
-    <div>
-      <SearchBar onSearch={handleSearch} />
-      <div className="grid grid-cols-2 gap-4 p-4">
-        {movies.map((movie) => (
-          <MovieCard key={movie.imdbID} movie={movie} onClick={() => {}} />
-        ))}
+    <div className="container mx-auto p-4">
+      <SearchBar setMovies={setMovies} setError={setError} />
+      {error && <div className="text-red-500">{error}</div>}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        {movies.length > 0 ? (
+          movies.map(movie => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))
+        ) : (
+          <div>No movies found.</div>
+        )}
       </div>
     </div>
   );
