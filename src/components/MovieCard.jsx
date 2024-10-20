fb992ed49c6db3b1395b3e0613864c4c70ab0f7e
Roaa <roaabbass@gmail.com>
2024-10-20 18:56:39 +0300

    Proper code for the movie poster and title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
 
-const MovieCard = ({ movie }) => (
-  <Link to={`/movie/${movie.id}`} className="block">
-    <img src={movie.poster} alt={movie.title} className="rounded-md" />
-    <h3 className="text-white font-semibold">{movie.title}</h3>
-    <p className="text-gray-400">{movie.year}</p>
-  </Link>
-);
+import { Link } from 'react-router-dom';
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4">
+      <img src={movie.Poster} alt={movie.Title} className="w-full rounded-t-lg" />
+      <h2 className="font-bold mt-2">{movie.Title}</h2>
+      <p>{movie.Year}</p>
+      <Link to={`/details/${movie.imdbID}`} className="text-blue-500">View Details</Link>
+    </div>
+  );
+};
 
 export default MovieCard;
